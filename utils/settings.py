4ef9567686ba1765770c2cd4cb0b24117c94717a
Daniel <danieelpark@gmail.com>
2024-05-11 00:11:04 +0300

    Поменял название

--- utils/settings.py
+++ utils/settings.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 DATA_PATH = ROOT_PATH.joinpath('data')
-PROFESSIONS_JSON_PATH = DATA_PATH.joinpath('operations.json')
+OPERATIONS_JSON_PATH = DATA_PATH.joinpath('operations.json')
